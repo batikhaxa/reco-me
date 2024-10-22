eb60bedbe140f5e47ce5b5af05945ae385e4fe1d
batikhaxa <137380050+batikhaxa@users.noreply.github.com>
2024-10-22 09:50:41 +0900

    trying to install react ;(

--- src/app.js
+++ src/app.js
@@ -1,28 +1,17 @@
-// src/App.js
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import './App.css'; // You can create a CSS file for additional styles
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './App.css'; 
+import Sidebar from './sidebar';
 
 const App = () => {
     return (
-        <main className="d-flex flex-nowrap full-height">
-            {/* Sidebar */}
-            <aside className="sidebar">
-                {/* You can add sidebar content here */}
-            </aside>
-
-            {/* Main Content */}
-            <div className="flex-fill p-3" style={{ backgroundColor: 'aqua' }}>
-                <div className="row">
-                    <div className="col border border-primary">
-                        Friend Recommendation Container
-                    </div>
-                    <div className="col border border-primary">
-                        Secondary Friend Recommendation
-                    </div>
-                </div>
-            </div>
-        </main>
+        <div className="app">
+            <Sidebar />  {/* Use Sidebar component */}
+            <main>
+                {/* Main content goes here */}
+                <h1>Main Content Area</h1>
+            </main>
+        </div>
     );
 };
 
