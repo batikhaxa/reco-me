8fb4ceedf14c70bb9e0bf5736fbd7e5d64df793d
batikhaxa <137380050+batikhaxa@users.noreply.github.com>
2024-10-22 10:34:24 +0900

    eeeeeeeeee

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import Main from './Main';
 
+console.log("loading app.js"); 
+
 const App = () => {
     return (
         <main className="d-flex flex-nowrap full-height">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+console.log("loading index.js"); 
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
