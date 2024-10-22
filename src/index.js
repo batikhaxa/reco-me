278e8e442a0e46460957e54aa814c6ae3721114e
batikhaxa <137380050+batikhaxa@users.noreply.github.com>
2024-10-22 11:12:45 +0900

    ioo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/style.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
