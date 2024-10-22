ea96ebe795bab74429b677a2b6e914231224944f
batikhaxa <137380050+batikhaxa@users.noreply.github.com>
2024-10-22 10:58:23 +0900

    yattttaaa

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
+// src/App.js
 import React from 'react';
-import Sidebar from './Sidebar';
-import Main from './Main';
+import Sidebar from './components/Sidebar';
+import Main from './components/Main';
 
-console.log("loading app.js"); 
-
-const App = () => {
+function App() {
     return (
         <main className="d-flex flex-nowrap full-height">
             <Sidebar />
             <Main />
         </main>
     );
-};
+}
 
 export default App;
