4aaacfaedd889c354988d969b6b25fda52a132c2
batikhaxa <137380050+batikhaxa@users.noreply.github.com>
2024-10-29 10:21:03 +0900

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -10,6 +10,12 @@ const Main = () => {
                     <div className="d-flex justify-content-center">
                         <div className="card" style={{ width: "30rem", height: "15rem" }}>
                             <div className="card-body">
+<<<<<<< Updated upstream
+=======
+                                <div className='d-flex align-items-center gap-2'>
+                            <img src="https://i.pinimg.com/736x/d1/51/47/d151474e2d22d12412425632275f6cff.jpg" alt="" width="70"
+                        height="70" className="rounded-circle me-2" />
+>>>>>>> Stashed changes
                                 <h5 className="card-title"><b>tanaka-san</b> is recommending <b>top gun</b></h5>
                                 <p className="card-text">Recommendention text</p>
                                 <label className="card-category mb-3">ANIME SF etc</label>
