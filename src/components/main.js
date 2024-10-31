69203d4ce12ad47766bf5f6c19249061ed09a4e3
Piyah11 <138852521+Piyahathai11@users.noreply.github.com>
2024-10-31 11:07:55 +0900

    no

--- src/components/main.js
+++ src/components/main.js
@@ -24,7 +24,12 @@ const Main = () => {
                                 <label className="card-category mb-3"><i class="bi bi-tv"></i>anime</label>
 
                               
+<<<<<<< HEAD
                         <div className='d-flex flex-row-reverse' >
+=======
+                                
+                                <div className='d-flex flex-row-reverse' >
+>>>>>>> parent of c9fac26 (very dangerous oroablt throws error)
 
                                 <a href="/comments" className='link-icon'>
                                 <i ><svg xmlns="http://www.w3.org/2000/svg"
@@ -45,55 +50,14 @@ const Main = () => {
                     </div>
                    
                     <br />
-                    <div className="d-flex justify-content-center">
-                        <div className="card" style={{ width: "25rem"}}>
-                            <div className="card-body">
-
-                            <div className='d-flex gap-2'>
-                            <img src="https://i.pinimg.com/736x/d1/51/47/d151474e2d22d12412425632275f6cff.jpg" alt="" width="70"
-                        height="70" className="rounded-circle me-2" />
-                                <h5 className="card-title"><b>b-san</b> is recommending <b>top gun</b></h5>
-                                </div>
-                                <p className="card-text">Recommendention text</p>
-                                <label className="card-category mb-3"><i class="bi bi-tv"></i>anime</label>
-                                <div className='d-flex flex-row-reverse'>
-
-                                
-                                <a href="/comments" className='link-icon'>
-                                <i ><svg xmlns="http://www.w3.org/2000/svg"
-                            width="30"
-                            height="30"
-                            fill="black"
-                            class="bi bi-chat-dots"
-                            viewBox="0 0 16 16">
-                            <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
-                            <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2"/>
-                            </svg></i>
+                 
+                           
                                 
                                    
-                                </a>
+                                
                                 </div>
-
-                            <div class="d-flex align-items-center gap-2">
-                            <img src="https://i.pinimg.com/736x/d1/51/47/d151474e2d22d12412425632275f6cff.jpg" alt="" width="50"
-                        height="50" className="rounded-circle me-2" />
-                                 <div className="dl-flex flex-column">
-                                <h5 className="card-title mb-2">XXX person is Recommending</h5>
-                               
-                              
-                                <h6 className="card-subtitle mb-2 ">What thing</h6>
-                                </div>
-                                </div>
-                                <p className="card-text">Recommendention text</p>
-                                <label className="card-category mb-3">ANIME SF etc</label>
-                                <div className=" d-flex flex-row-reverse">
-                                <a href="/comments" className="card-link mb-3">COMMENTS</a>
-                                </div>
-                          
-
-                            </div>
-                        </div>
-                    </div>
+                                <br/>
+                         
                 </div>
                
                 <div className="col d-none d-sm-block d-sm-none d-md-block ">
@@ -213,7 +177,7 @@ const Main = () => {
                     </div>
                 </div>
             </div>
-        </div>
+       
        
     );
 };
